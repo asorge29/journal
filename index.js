3f47ffb972c819a6e18677395bf7888d1109c099
Python Darklord <pythondarklord@gmail.com>
2025-02-12 18:44:27 -0800

    Journal Image

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1800,
+        height: 1300,
+        transparent: true,
+        // frame: false
     })
 
     win.loadFile('index.html')
