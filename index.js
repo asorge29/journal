af2d26d7eef7afc9f4fbde1ebddad32e0f4573a8
Python Darklord <pythondarklord@gmail.com>
2025-02-12 20:04:25 -0800

    Screen size adapting

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, screen } = require('electron')
+
+let win = null;
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1800,
-        height: 1300,
-        transparent: true,
-        frame: false
-    })
-
+    const relWinSize = 400;
+    const primaryDisplay = screen.getPrimaryDisplay()
+    const {width, height} = primaryDisplay.workAreaSize;
+        win = new BrowserWindow({
+            width: width-relWinSize*2.8,
+            height: height-relWinSize,
+            transparent: true,
+            // frame: false
+        })
     win.loadFile('index.html')
 }
 
